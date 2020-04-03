71298164deb589cd3e0d3b8c75758c23ae3592d5
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-04-03 10:06:07 -0400

    setup  routes

---
+++ Develop/models/workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercise:[{
+  type: {
+    type: String,
+    trim: true,
+    required: "Enter type of exercise either resistance or cardio"
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "enter name of exercise"
+  },
+  duration:{
+    type:Number,
+    required:"Enter minutes for exercise"
+  }, 
+  weight:{
+    type: Number
+  },
+  reps:{
+    type:Number
+  },
+  sets:{
+    type:Number
+  },
+  distance:{
+    type:Number
+  }
+
+}]
+});
+
+const Workout= mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
+
