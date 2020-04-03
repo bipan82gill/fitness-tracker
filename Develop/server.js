87f4649d250b5a0d6d0f87d0cdd73b70c0d63b5b
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-04-03 11:05:25 -0400

    install morgan package

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
@@ -16,7 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/api.js"));
 app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
